590d0c14b1f2d6981ca6584da3224265159300c7
CallaJun <maracondo@aol.com>
2015-01-17 19:59:29 -0500

    Fixed bugs, added Chart.js

--- ex.py
+++ ex.py
@@ -22,6 +22,9 @@ class ProgressHandler(webapp2.RequestHandler):
     template = jinja_environment.get_template('progress.html')
     self.response.out.write(template.render(template_values))
 
+jinja_environment = jinja2.Environment(loader=
+  jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 class TrackHandler(webapp2.RequestHandler):
   def get(self):
     renderedHeader = header.getHeader(users.get_current_user().user_id())
